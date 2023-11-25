6ac10a5adeb95c7e1ce103e85b060f610b28f75e
sukes13 <sir_jelle_wouters@hotmail.com>
2023-11-25 23:13:49 +0100

    Can handle insert of all valid and invalid coins - basic

--- src/test/kotlin/com/sukes13/codekata/VendingMachineTest.kt
+++ src/test/kotlin/com/sukes13/codekata/VendingMachineTest.kt
@@ -3,33 +3,80 @@ package com.sukes13.codekata
 import com.sukes13.codekata.CoinRegistry.valueOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class VendingMachineTest {
 
-    @Test
-    fun `When inserting a valid coin, value is saved in machine`() {
-        val actual = VendingMachine().insert(COIN_ONE_CENT)
+    @ParameterizedTest
+    @MethodSource("allValidCoinsTest")
+    fun `When inserting a valid coin, value is amounted in machine`(coin: Coin, amount: Double) {
+        val actual = VendingMachine().insert(coin)
 
-        assertThat(actual.amount).isEqualTo(0.01)
+        assertThat(actual.amount).isEqualTo(amount)
+    }
+
+    companion object {
+        @JvmStatic
+        fun allValidCoinsTest(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(COIN_ONE_CENT, 0.01),
+                Arguments.of(COIN_TWO_CENT, 0.02),
+                Arguments.of(COIN_FIVE_CENT, 0.05),
+                Arguments.of(COIN_TEN_CENT, 0.1),
+                Arguments.of(COIN_TWENTY_CENT, 0.2),
+                Arguments.of(COIN_FIFTY_CENT, 0.5),
+                Arguments.of(COIN_ONE_EURO, 1.0),
+                Arguments.of(COIN_TWO_EURO, 2.0),
+            )
+    }
+
+    @Test
+    fun `When inserting an invalid coin, value is not saved and coin is returned`() {
+        val invalidCoin = COIN_ONE_CENT.copy(diameter = 2.00)
+        val actual = VendingMachine().insert(invalidCoin)
+
+        assertThat(actual.amount).isEqualTo(0.0)
+        assertThat(actual.coinChute).containsExactly(invalidCoin)
     }
 }
 
-data class VendingMachine(val amount: Double = 0.0) {
-    fun insert(coin: Coin) = copy(amount = amount + coin.valueOf())
+data class VendingMachine(
+    val amount: Double = 0.0,
+    val coinChute: List<Coin> = emptyList()
+) {
+    fun insert(coin: Coin) =
+        coin.valueOf()
+            ?.let { copy(amount = amount + it) }
+            ?: copy(coinChute = coinChute + coin)
+
 }
 
-
 val COIN_ONE_CENT = Coin(16.25, 1.67, 2.30)
+val COIN_TWO_CENT = Coin(18.75, 1.67, 3.06)
+val COIN_FIVE_CENT = Coin(21.25, 1.67, 3.92)
+val COIN_TEN_CENT = Coin(19.75, 1.93, 4.10)
+val COIN_TWENTY_CENT = Coin(22.25, 2.14, 5.74)
+val COIN_FIFTY_CENT = Coin(24.25, 2.38, 7.80)
+val COIN_ONE_EURO = Coin(23.25, 2.33, 7.50)
+val COIN_TWO_EURO = Coin(25.75, 2.20, 8.50)
 
 object CoinRegistry {
     private val registry = mapOf(
-        COIN_ONE_CENT to 0.01
+        COIN_ONE_CENT to 0.01,
+        COIN_TWO_CENT to 0.02,
+        COIN_FIVE_CENT to 0.05,
+        COIN_TEN_CENT to 0.1,
+        COIN_TWENTY_CENT to 0.2,
+        COIN_FIFTY_CENT to 0.5,
+        COIN_ONE_EURO to 1.0,
+        COIN_TWO_EURO to 2.0,
     )
 
-    fun Coin.valueOf() = registry[this] ?: throw InvalidCoinException()
+    fun Coin.valueOf() = registry[this]
 }
-
-class InvalidCoinException : Exception()
 
 data class Coin(
     private val diameter: Double,
