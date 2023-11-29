0ffc061d16e960bdeaa89d45632c0be03045c52b
sukes13 <sir_jelle_wouters@hotmail.com>
2023-11-29 21:50:27 +0100

    Refactorings

--- src/main/kotlin/com/sukes13/vendingmachine/EventStore.kt
+++ src/main/kotlin/com/sukes13/vendingmachine/EventStore.kt
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 
 sealed interface VendingEvent {
-    class AmountInsertedEvent(val amount: Double) : VendingEvent
+    class ValueInsertedEvent(val amount: Double) : VendingEvent
     class CoinReturnedEvent(val coin: Coin) : VendingEvent
 
     sealed class TimedVendingEvent : VendingEvent {
