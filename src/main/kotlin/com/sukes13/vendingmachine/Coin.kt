176bf6fa8a498e9bfbc091484e97336a8fd2125e
sukes13 <sir_jelle_wouters@hotmail.com>
2023-12-02 21:21:57 +0100

    Working returning correct change

--- src/main/kotlin/com/sukes13/vendingmachine/Coin.kt
+++ src/main/kotlin/com/sukes13/vendingmachine/Coin.kt
@@ -1,26 +1,29 @@
 package com.sukes13.vendingmachine
 
-val COIN_ONE_CENT = Coin(16.25, 1.67, 2.30)
-val COIN_TWO_CENT = Coin(18.75, 1.67, 3.06)
-val COIN_FIVE_CENT = Coin(21.25, 1.67, 3.92)
-val COIN_TEN_CENT = Coin(19.75, 1.93, 4.10)
-val COIN_TWENTY_CENT = Coin(22.25, 2.14, 5.74)
-val COIN_FIFTY_CENT = Coin(24.25, 2.38, 7.80)
-val COIN_ONE_EURO = Coin(23.25, 2.33, 7.50)
-val COIN_TWO_EURO = Coin(25.75, 2.20, 8.50)
+val COIN_ONE_CENT = Coin("1 cent", 16.25, 1.67, 2.30)
+val COIN_TWO_CENT = Coin("2 cent", 18.75, 1.67, 3.06)
+val COIN_FIVE_CENT = Coin("5 cent", 21.25, 1.67, 3.92)
+val COIN_TEN_CENT = Coin("10 cent", 19.75, 1.93, 4.10)
+val COIN_TWENTY_CENT = Coin("20 cent", 22.25, 2.14, 5.74)
+val COIN_FIFTY_CENT = Coin("50 cent", 24.25, 2.38, 7.80)
+val COIN_ONE_EURO = Coin("1 euro", 23.25, 2.33, 7.50)
+val COIN_TWO_EURO = Coin("2 euro", 25.75, 2.20, 8.50)
 
 data class Coin(
+    private val name: String,
     private val diameter: Double,
     private val thickness: Double,
-    private val mass: Double
-)
+    private val mass: Double,
+) {
+    override fun toString() = "Coin($name)"
+}
 
 object CoinRegistry {
     private val registry = mapOf(
         COIN_ONE_CENT to 0.01,
         COIN_TWO_CENT to 0.02,
         COIN_FIVE_CENT to 0.05,
-        COIN_TEN_CENT to 0.1,
+        COIN_TEN_CENT to 0.10,
         COIN_TWENTY_CENT to 0.2,
         COIN_FIFTY_CENT to 0.5,
         COIN_ONE_EURO to 1.0,
@@ -28,11 +31,19 @@ object CoinRegistry {
     )
 
     fun Coin.value() = registry[this]
-    fun inCoins(remainder: Double, coins: List<Coin> = emptyList()): List<Coin> {
-        if (remainder <= 0) {
-            val highestValueInRemainder = registry.entries.maxBy { (_, value) -> value <= remainder }
-            inCoins(remainder - highestValueInRemainder.value, coins.plus(highestValueInRemainder.key))
+
+    tailrec fun inCoins(remainder: Double, coins: List<Coin> = emptyList()): List<Coin> {
+        if (remainder <= 0.0) {
+            return coins
         }
-        return coins
+        val highestValueInRemainder = registry.maxBy { (_, value) ->
+            if ((remainder.minusPrecise(value)) >= 0.0) value else 0.0
+        }
+        return inCoins(
+            remainder.minusPrecise(highestValueInRemainder.value),
+            coins + highestValueInRemainder.key
+        )
     }
+
+    private fun Double.minusPrecise(second: Double) = (toBigDecimal() - second.toBigDecimal()).toDouble()
 }
--- src/test/kotlin/com/sukes13/vendingmachine/CoinRegistryTest.kt
+++ src/test/kotlin/com/sukes13/vendingmachine/CoinRegistryTest.kt
@@ -2,11 +2,27 @@ package com.sukes13.vendingmachine
 
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class CoinRegistryTest {
-    @Test
-    fun `When amount is provided, get it in as least coins as possible`() {
-        assertThat(CoinRegistry.inCoins(1.0)).containsExactly(COIN_ONE_EURO)
+
+    @ParameterizedTest
+    @MethodSource("inCoinsInput")
+    fun `When amount is provided, get it in as least coins as possible`(inputValue: Double, coinResult: List<Coin>) {
+        assertThat(CoinRegistry.inCoins(inputValue)).containsExactlyInAnyOrder(*coinResult.toTypedArray())
+    }
+
+    companion object {
+        @JvmStatic
+        fun inCoinsInput(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(1.0, listOf(COIN_ONE_EURO)),
+                Arguments.of(1.01, listOf(COIN_ONE_EURO, COIN_ONE_CENT)),
+                Arguments.of(1.11, listOf(COIN_ONE_EURO, COIN_TEN_CENT, COIN_ONE_CENT)),
+                Arguments.of(3.61, listOf(COIN_TWO_EURO,COIN_ONE_EURO, COIN_FIFTY_CENT,COIN_TEN_CENT, COIN_ONE_CENT)),
+            )
     }
 }
