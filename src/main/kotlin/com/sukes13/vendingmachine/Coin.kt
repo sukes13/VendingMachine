33e2a4f82716da67137434655e401c11bc9d655f
Jelle Wouters <jelle.wouters@sfpd.fgov.be>
2024-10-09 15:50:45 +0200

    Refactorings for simplicity and readability

--- src/main/kotlin/com/sukes13/vendingmachine/Coin.kt
+++ src/main/kotlin/com/sukes13/vendingmachine/Coin.kt
@@ -33,6 +33,10 @@ object CoinRegistry {
 
     fun Coin.value() = registry[this]
 
+    fun List<Coin>.minusCoins(coins: List<Coin>): List<Coin> = coins
+        .fold(this) { coinsInList, coinToRemove -> coinsInList.minusElement(coinToRemove) }
+
+
     tailrec fun inCoins(remainder: Double, coins: List<Coin> = emptyList()): List<Coin> {
         if (remainder <= 0.0) {
             return coins
