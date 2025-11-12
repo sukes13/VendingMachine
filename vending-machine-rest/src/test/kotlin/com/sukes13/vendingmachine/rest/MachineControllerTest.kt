149635f21b30fc6ad630807dd3deac02e17363e2
sukes13 <sir_jelle_wouters@hotmail.com>
2025-11-12 15:25:53 +0100

    Working maven project

--- vending-machine-rest/src/test/kotlin/com/sukes13/vendingmachine/rest/MachineControllerTest.kt
+++ vending-machine-rest/src/test/kotlin/com/sukes13/vendingmachine/rest/MachineControllerTest.kt
@@ -1,5 +1,8 @@
 package com.sukes13.vendingmachine.rest
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+
 @AutoConfigureMockMvc
 @SpringBootTest()
 class MachineControllerTest {
